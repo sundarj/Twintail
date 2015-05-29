bdf4d4be702f69205613861ba04c83990f1a31a0
nallown <nasir-1996@hotmail.co.uk>
2015-05-29 15:12:55 +0100

    include different format of heading 1 and 2

--- tests/headings.js
+++ tests/headings.js
@@ -6,11 +6,19 @@ String.prototype.repeat = function(n) {
 }
 
 describe('Headings', function() {
-    
+
     for (var i=1; i<=6; i++) {
         it('should render a h'+i + ' element', function() {
-            expect(twintail.render('#'.repeat(i) + ' title')).to.equal('<hn>title</hn>\n'.replace(/hn/g, 'h'+i)); 
+            expect(twintail.render('#'.repeat(i) + ' title')).to.equal('<hn>title</hn>\n'.replace(/hn/g, 'h'+i));
         });
     }
-    
+
+    it('should render a h1 element', function() {
+      expect(twintail.render('title\n=====')).to.equal('<h1>title</h1>\n');
+    });
+
+    it('should render a h2 element', function() {
+      expect(twintail.render('title\n-----')).to.equal('<h2>title</h2>\n');
+    });
+
 });
