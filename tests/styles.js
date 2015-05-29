e4139dc525db587bb111382eb8778fe0f5c5ffca
nallown <nasir-1996@hotmail.co.uk>
2015-05-29 16:07:36 +0100

    rename style to styles

---
+++ tests/styles.js
@@ -0,0 +1,16 @@
+var twintail = require('../Twintail.js');
+var expect = require('expect.js');
+
+describe('Styles', function() {
+    it('should render a bold element', function() {
+      expect(twintail.render('*bold*')).to.equal('<p><strong>bold</strong></p>\n');
+    });
+
+    it('should render a italic element', function() {
+      expect(twintail.render('_italic_')).to.equal('<p><em>italic</em></p>\n');
+    });
+
+    it('should render a strikethrough element', function() {
+      expect(twintail.render('-strikethrough-')).to.equal('<p><strike>strikethrough</strike></p>\n');
+    });
+});
